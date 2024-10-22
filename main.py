c80a778b246b0b1e657742e104587bbc2bdad861
Joe Scruppi <jscruppi@gmail.com>
2024-10-22 00:14:44 -0500

    Fixed inline comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Script to make the contact .vcf file
 
 
 Last Updated: October 21, 2024
+Authors: Joe Scruppi
 
 """
 
@@ -14,39 +15,34 @@ def addContact(f, c):
     #write the header
     f.write('BEGIN:VCARD\n')
     f.write('VERSION:3.0\n')
+
+    #write the contact contents
     print(f"N:{c[2]};{c[1]};;;", file=f)
     print(f"TEL:{c[4]}", file=f)
     print(f"BDAY:{c[3]}", file=f)
     if c[5] != "":
         print(f"PHOTO;VALUE=URL:{c[5]}",file=f)
+
+    #write the footer
     f.write('END:VCARD\n')
     f.write('\n')
-    
-    
-    
 
 def main():
-    print('in main')
     
     #create vcf file
     out_file = open('output.vcf', mode='w')
 
-    #read through the input file
+    #read through the input csv file
     with open(sys.argv[1], mode='r') as input_file:
         reader = csv.DictReader(input_file, delimiter=',')
         
         for row in reader:
-            #split each current contact
-            #contact = row.split(',')
+            #split each current contact row into their attributes
             contact = list(row.values())
-            #print(contact, type(contact))
+            #write the data to the file
             addContact(out_file, contact)
-
-
-
 
 #when script is run
 if __name__ == '__main__':
     main()
-
 
