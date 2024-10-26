c55f261033eccbc58bd5e2e4ba234685c23ec9a0
Joe Scruppi <jscruppi@gmail.com>
2024-10-25 19:07:57 -0500

    File-paths shortened

--- contact-maker/src/window.py
+++ contact-maker/src/window.py
@@ -12,6 +12,7 @@ import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from .contact_process import *
 import sys
+import os
 
 class Window:
     def __init__(self):
@@ -34,7 +35,7 @@ class Window:
 
         self.gen_button = tk.Button(self.root, text='Generate Contact File')#command=s.genFile(self.output_file_path,  self.input_file_path))
         self.gen_button.pack()
-        self.output_file_label = tk.Label(self.root, text=self.output_file_path)
+        self.output_file_label = tk.Label(self.root, text=self.shorten_path(self.output_file_path))
         self.output_file_label.pack()
 
         #set the icon image
@@ -53,13 +54,18 @@ class Window:
         self.input_file_path = filedialog.askopenfilename()
         if self.input_file_path:
             #display input path on the window screen
-            self.input_file_label.config(text=f'{self.input_file_path}')
+            self.input_file_label.config(text=self.shorten_path(self.input_file_path))
 
             #enable genFile button to function now that we have the loaded path
             #the lambda call makes it so command will only activate once the button is clicked
             self.gen_button.config(command= lambda: genFile(self.output_file_path, self.input_file_path))
         else:
             messagebox.showwarning("No file Selected", "You did not choose the CSV file")
+
+    def shorten_path(self, path, num_parts=2):
+        parts = path.split(os.sep)
+        shortened = os.sep.join(parts[-num_parts:])
+        return shortened
 
     def run(self):
         self.root.mainloop()
