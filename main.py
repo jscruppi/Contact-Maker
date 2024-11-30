007b4eaa7923091f4c0de1419e2038f5d0a441da
Joe Scruppi <jscruppi@gmail.com>
2024-11-30 17:03:28 -0600

    Added choice to launch Gui or Cmdline based off of args

--- main.py
+++ main.py
@@ -7,15 +7,28 @@ Authors: Joe Scruppi
 
 """
 
-from src import window
+from src import window, contact_process
+import sys
 
-def main():
+def main() -> None:
 
-    #create the window
-    w = window.Window()
+    if len(sys.argv) == 1:
+        #create the window
+        w = window.Window()
 
-    #run the mainloop
-    w.run()
+        #run the mainloop
+        w.run()
+    else:
+        contact_process.genFile(sys.argv[1], 'out/output.vcf')
+
+        with open('data/omit_names.txt', mode='r') as err_log:
+            print('Names not included in the file:')
+            lines = err_log.readlines()
+            for line in lines:
+                print(f'\t{line}')
+
+        err_log.close()
+
 
 #when script is run
 if __name__ == '__main__':
